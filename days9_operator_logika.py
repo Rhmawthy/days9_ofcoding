304cfcdf01688247744c8fb3815d71237770bb5c
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-19 04:32:34 -0700

    Add files via upload

---
+++ days9_operator_logika.py
@@ -0,0 +1,42 @@
+#operator logika
+#ada 3 jenis operator logika yaitu and, or, dan not
+
+# AND, ia akan bernilai true ketika dua duanya true
+
+print( " AND ")
+a = True
+b = True
+print(a and b)
+
+a = False
+b = True
+print(a and b)
+
+a = False
+b = False
+print(a and b)
+
+# OR, jika salah satunya true maka nilainya true
+
+print ( " OR " )
+a = True
+b = True
+print(a or b)
+
+a = True
+b = False
+print(a or b)
+
+a = False
+b = False
+print(a or b)
+
+#NOT,ketika ia true maka nilainya false begitupun sebaliknya
+
+print ( " NOT " )
+a = True
+b = False
+print(not a , not b)
+
+
+
